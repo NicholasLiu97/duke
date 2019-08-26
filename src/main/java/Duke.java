d950e9e4bee3dbef592b982c2b0490797ef0fad4
Nicholas Liu <nicholasliu097@gmail.com>
2019-08-26 17:36:20 +0800

    added level-1 features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static void echo(String cmd) {
+        System.out.println(cmd);
+    }
     public static void main(String[] args) {
+/*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+ */
+
+        String cmd;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        while(true) {
+            cmd = scanner.next();
+            if (cmd.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                echo(cmd);
+            }
+        }
     }
+
+
 }
+
