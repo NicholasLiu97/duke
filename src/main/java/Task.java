7dde07f7fb2dfae0296465007df139c0bb1b25d5
Nicholas Liu <nicholasliu097@gmail.com>
2019-08-26 21:45:24 +0800

    added Task.java

---
+++ src/main/java/Task.java
@@ -0,0 +1,16 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        if (isDone) {
+            return "\u2713";
+        }
+        return "\u2718";
+    }
+}
