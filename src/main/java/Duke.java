08623d718558cf2c5227a4301af7389244f6f8a2
Nicholas Liu <nicholasliu097@gmail.com>
2019-08-26 21:46:23 +0800

    added level-3 features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,30 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
 
-    private static ArrayList<String> list = new ArrayList<>();
+    private static ArrayList<Task> list = new ArrayList<>();
+
+    private static void showList() {
+        for (int i = 0; i < list.size(); i++) {
+            Task currTask = list.get(i);
+            System.out.println((i+1) + ".[" + currTask.getStatusIcon() + "] " + currTask.description);
+        }
+    }
     private static void addToList(String cmd) {
-        list.add(cmd);
+        Task newTask = new Task(cmd);
+        list.add(newTask);
         System.out.println("added: " + cmd);
     }
+
+    private static void markAsDone(String taskNumber) {
+        int taskNum = Integer.parseInt(taskNumber);
+        Task currTask = list.get(taskNum - 1);
+        currTask.isDone = true;
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(" [" + currTask.getStatusIcon() + "] " + currTask.description);
+    }
+
     public static void main(String[] args) {
 /*
         String logo = " ____        _        \n"
@@ -29,15 +45,16 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else if (cmd.equals("list")) {
-                for (int i = 0; i < list.size(); i++) {
-                    System.out.println((i + 1) + ". " + list.get(i));
+                showList();
+            } else { //either done a task or add a task
+                String[] words = cmd.split(" ");
+                if (words[0].equals("done")) { //done with a task
+                    markAsDone(words[1]);
+                } else { //adding task to list
+                    addToList(cmd);
                 }
-            } else {
-                addToList(cmd);
             }
         }
     }
-
-
 }
 
