e1d876bdb037582c18cbbd686c95da910460964e
Nicholas Liu <nicholasliu097@gmail.com>
2019-08-26 19:40:21 +0800

    added level-2 features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,12 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
 
-    private static void echo(String cmd) {
-        System.out.println(cmd);
+    private static void addToList(String cmd, ArrayList<String> list) {
+        list.add(cmd);
+        System.out.println("added: " + cmd);
     }
     public static void main(String[] args) {
 /*
@@ -15,18 +18,23 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
  */
-
         String cmd;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+
+        ArrayList<String> list = new ArrayList<>();
         while(true) {
-            cmd = scanner.next();
+            cmd = scanner.nextLine();
             if (cmd.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (cmd.equals("list")) {
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println((i + 1) + ". " + list.get(i));
+                }
             } else {
-                echo(cmd);
+                addToList(cmd, list);
             }
         }
     }
